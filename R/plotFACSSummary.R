71340bad676c9378bee59d4cc3ee34b443639619
Andrew Wight <AndrewE_Wight@dfci.harvard.edu>
2021-12-14 15:00:10 -0500

    Okay so turns out waiting to long to enquo fillGroup was the problem, but now the null test failed because once quoted, fillGroup wasn't NULL anymore. Fixed it so we evaluate fillGroup and take the second component, which is NULL when fillGroup starts NULL and not otherwise.

--- R/plotFACSSummary.R
+++ R/plotFACSSummary.R
@@ -43,13 +43,10 @@ plotFACSSummary <- function(data, filterPop, ylab, xGroup = .data$Genotype, fill
                             ..., statbar = "nobar", fontFix = TRUE){
   xGroup <- ggplot2::enquo(xGroup)
   fillGroup <- ggplot2::enquo(fillGroup)
-  # if(!is.null(fillGroup)){
-  #   fillGroup <- ggplot2::enquo(fillGroup)
-  # }
 
   df <- data |>
     dplyr::filter(stringr::str_detect(.data$pop, filterPop))
-  if(is.null(fillGroup)){
+  if(is.null(eval(fillGroup)[[2]])){
     gg <- ggplot2::ggplot(df, ggplot2::aes(!!xGroup, .data$percent, fill = !!xGroup)) +
       ggplot2::stat_summary(geom = "errorbar") +
       ggplot2::geom_bar(stat = "summary", color = "black")
